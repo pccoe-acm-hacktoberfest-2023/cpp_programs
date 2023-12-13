2736d5f0b1528912b459dbf3ca40c1559b077637
patil-rushikesh <prushikeh@gmail.com>
2023-12-13 20:17:52 +0530

    Added a rectangular pattern code

---
+++ Patterns/rectangle_pattern.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //printing 4 by 5 stars in rectangle shape
+    for(int i=0; i<=4; i++){
+        for(int j = 0; j<=5; j++){
+            cout<<" * ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
