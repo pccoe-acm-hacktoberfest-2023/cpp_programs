78d7cad83c62dec1765f328605c616225fd24a2a
patil-rushikesh <prushikeh@gmail.com>
2023-12-14 11:51:27 +0530

    Added a program which prints the pyramid using numbers

---
+++ Patterns/half_pyramid_using_numbers.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int m;
+    cout<<"Enter the Number\n";
+    cin>>m;
+    for(int i = 1; i<=m; i++){
+        for(int j=1; j<=i; j++){
+            cout<<i;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
