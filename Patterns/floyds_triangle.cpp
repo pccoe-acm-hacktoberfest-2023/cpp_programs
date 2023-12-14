fe73afeec37636d08fd6eab548f913a3af7ab10b
patil-rushikesh <prushikeh@gmail.com>
2023-12-14 12:00:30 +0530

    Corrected space issue while priniting pattern

--- Patterns/floyds_triangle.cpp
+++ Patterns/floyds_triangle.cpp
@@ -1,12 +1,15 @@
+
 #include<iostream>
 using namespace std;
 int main(){
     int m;
     cout<<"Enter the Number\n";
     cin>>m;
+    int n=1;
     for(int i = 1; i<=m; i++){
         for(int j=1; j<=i; j++){
-            cout<<i;
+            cout<<n<<" ";
+            n++;
         }
         cout<<endl;
     }
