7a578b1ecd294e9453b5abbc18063083fa7b300d
Rushikesh Patil <prushikeh@gmail.com>
2023-12-20 23:27:37 +0530

    Added a program in class folder

--- class/bin_op_overload.cpp
+++ class/bin_op_overload.cpp
@@ -1,56 +1,60 @@
-#include <iostream>
-
-class ClassB;  // Forward declaration
-
-class ClassA {
-private:
-    int valueA;
-
-public:
-    ClassA(int valA) : valueA(valA) {}
-
-    friend void swapValues(ClassA& objA, ClassB& objB);  // Friend function declaration
-
-    void display() const {
-        std::cout << "ClassA: " << valueA << std::endl;
+#include<iostream>
+using namespace std;
+class fraction
+{
+    private: float n,d;
+    public: fraction()
+    {
+        n =1;
+        d=1;
     }
+    fraction operator+(fraction);
+    fraction operator/(fraction);
+    void getfr(void);
+    void printfr(void);
 };
-
-class ClassB {
-private:
-    int valueB;
-
-public:
-    ClassB(int valB) : valueB(valB) {}
-
-    friend void swapValues(ClassA& objA, ClassB& objB);  // Friend function declaration
-
-    void display() const {
-        std::cout << "ClassB: " << valueB << std::endl;
-    }
-};
-
-// Friend function definition
-void swapValues(ClassA& objA, ClassB& objB) {
-    int temp = objA.valueA;
-    objA.valueA = objB.valueB;
-    objB.valueB = temp;
+fraction fraction::operator +(fraction f2)
+{
+    fraction r;
+    r.n = n*f2.d + d*f2.n;
+    r.d = d*f2.d;
+    return (r);
 }
-
-int main() {
-    ClassA objA(10);
-    ClassB objB(20);
-
-    std::cout << "Before swapping:" << std::endl;
-    objA.display();
-    objB.display();
-
-    // Call the friend function to swap values
-    swapValues(objA, objB);
-
-    std::cout << "\nAfter swapping:" << std::endl;
-    objA.display();
-    objB.display();
-
+fraction fraction::operator /(fraction f2)
+{
+    fraction r;
+    r.n = n*f2.d;
+    r.d = d*f2.n;
+    return (r);
+}
+void fraction::getfr(void)
+{
+    cout<<"\nEnter the numerator -->";
+    cin>>n;
+    cout<<"Enter the denominator -->";
+    cin>>d;
+}
+void fraction::printfr(void)
+{
+    long double r;
+    r = double (n/d);
+    cout<<n<<"/"<<d<<"="<<r<<" ";
+}
+int main()
+{
+    fraction f1,f2,f3,f4;
+    f1.getfr();
+    f2.getfr();
+    f3 = f1+f2;
+    f4 = f1/f2;
+    cout<<"\nFraction 1/t -->";
+    f1.printfr();
+    cout<<"\nFraction 2/t -->";
+    f2.printfr();
+    cout<<"\nSum\t\t -->";
+    f3.printfr();
+    cout<<"\nDivision\t -->";
+    f4.printfr();
+    cout<<endl;
     return 0;
 }
